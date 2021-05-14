5d0fbd84c9dca553f53f3c1fa17ffbb0f6809354
Alexandre_agamah <agamah21@gmail.com>
2021-05-14 10:13:08 +0200

    first_commit

---
+++ src/main/java/fr/da/miabebank/MiabeBankApplication.java
@@ -0,0 +1,13 @@
+package fr.da.miabebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiabeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiabeBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/da/miabebank/MiabeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.da.miabebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiabeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
